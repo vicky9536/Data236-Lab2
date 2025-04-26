a896d3e841ccb7f579a157b5ca5ddf6794946a3d
vicky9536 <nairui1212@gmail.com>
2025-04-26 13:17:48 -0700

    remove original backend folder

--- backend/routes/cusProfileRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const cusProfileCtrl = require('../controllers/cusProfileCtrl');
-
-router.get('/viewCus/me', cusProfileCtrl.viewCusProfile);
-router.put('/updateCus/me', cusProfileCtrl.updateCusProfile);
-
-module.exports = router;
--- backend/routes/resProfileRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const resProfileCtrl = require('../controllers/resProfileCtrl');
-
-router.get('/restaurants/:name/profile', resProfileCtrl.viewRestInfo);
-router.put('/restaurants/:id/profile', resProfileCtrl.updateRestInfo);
-
-module.exports = router;
